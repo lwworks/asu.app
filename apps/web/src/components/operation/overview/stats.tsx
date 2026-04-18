917da51a26fe3c4e663ac1c02392319b95e7d2ed
Lukas Wiesehan <lukas@lw.works>
2026-04-18 08:18:43 +0200

    tweak operation overview page and update auth worker timeout

--- apps/web/src/components/operation/overview/stats.tsx
+++ apps/web/src/components/operation/overview/stats.tsx
@@ -16,17 +16,15 @@ export const OperationStats = ({ operation }: { operation: Operation }) => {
     <Card className="p-0 grid grid-cols-4 gap-0">
       <div className="p-6 border-r">
         <div className="text-3xl font-mono">
-          {duration(operation.createdAt, currentTime)}
+          {duration(operation.createdAt, operation.completedAt ?? currentTime)}
         </div>
         <div className="text-sm uppercase tracking-wider text-muted-foreground">
           Einsatzdauer
         </div>
       </div>
       <div className="p-6 border-r">
-        <div className="text-3xl font-mono text-muted-foreground">
-          <span className="text-primary">{activeSquads.length}</span>
-          <span>/</span>
-          <span>{squads.length}</span>
+        <div className="text-3xl font-mono">
+          {activeSquads.length}
         </div>
         <div className="text-sm uppercase tracking-wider text-muted-foreground">
           Trupp{activeSquads.length !== 1 ? "s" : ""} eingesetzt
